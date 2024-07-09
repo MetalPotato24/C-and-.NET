754d44abc5c9f1e229c3c9b7c8535166ee12e2b4
Anika Islam <86525526+MetalPotato24@users.noreply.github.com>
2024-07-09 10:21:26 +0600

    .

--- obj/Debug/net8.0/C#.AssemblyInfo.cs
+++ obj/Debug/net8.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03297dfb1b9f66cfac9ab50c6c9356bf22f23451")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30973b6e6fe4311e6bf016cde124447b2e652743")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
